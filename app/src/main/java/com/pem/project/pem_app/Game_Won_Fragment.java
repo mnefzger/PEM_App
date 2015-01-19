63c0c0df5e9c7f46ed04b679dde8bbb7dad6ce89
mnefzger <matthiasnefzger.mail@gmail.com>
2015-01-19 21:11:30 +0100

    better won layout

--- app/src/main/java/com/pem/project/pem_app/Game_Won_Fragment.java
+++ app/src/main/java/com/pem/project/pem_app/Game_Won_Fragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -23,6 +24,8 @@ public class Game_Won_Fragment extends Fragment {
 
     private String wonKey;
     private int player;
+
+    private ImageView image;
 
 
     public static Game_Won_Fragment newInstance(String param1, int param2) {
@@ -51,6 +54,8 @@ public class Game_Won_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_game__won_, container, false);
+        image = (ImageView) v.findViewById(R.id.trophy);
+        setTrophy(wonKey);
         TextView message = (TextView)v.findViewById(R.id.wonMessage);
         if(message != null) message.setText("Congratulations!\nOn the wayside, you found the " + convertKey(wonKey) + "!");
 
@@ -89,6 +94,14 @@ public class Game_Won_Fragment extends Fragment {
         if(key.contains("coin")) return "amulet";
 
         return "";
+    }
+
+    public void setTrophy(String key){
+        if(key.equals("keyBlue")) image.setImageResource(R.drawable.key_blue_yes);
+        if(key.equals("keyYellow")) image.setImageResource(R.drawable.key_yellow_yes);
+        if(key.equals("keyGreen")) image.setImageResource(R.drawable.key_green_yes);
+        if(key.equals("keyRed")) image.setImageResource(R.drawable.key_red_yes);
+        if(key.contains("coin")) image.setImageResource(R.drawable.coinboth);
     }
 
 
